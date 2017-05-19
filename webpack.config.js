beaf4e74684f1858b5b6e38a3abb9d3af48813bf
Nathan Pickard <nathan.pickard87@gmail.com>
2017-05-18 19:31:13 -0700

    Babel, React, and SCSS loaders added

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,21 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
+    ]
   }
 };
